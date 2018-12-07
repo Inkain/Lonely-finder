9d189616c5db72071185ee11dbc59ad66f04ed12
laruson <chernikovichandrey@gmail.com>
2018-12-07 22:41:00 +0300

    map settings

---
+++ presentation/src/main/java/inkant1990/com/lonelyfinder/screen/map/MapRouter.kt
@@ -0,0 +1,6 @@
+package inkant1990.com.lonelyfinder.screen.map
+
+import inkant1990.com.lonelyfinder.base.BaseRouter
+
+class MapRouter(activity: MapActivity) : BaseRouter<MapActivity>(activity) {
+}
