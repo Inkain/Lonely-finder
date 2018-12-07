77e81df544c7c3c30b0a922ed02110a66cd6c0b2
laruson <chernikovichandrey@gmail.com>
2018-12-08 00:24:14 +0300

    add map

--- presentation/src/main/java/inkant1990/com/lonelyfinder/screen/map/MapActivity.kt
+++ presentation/src/main/java/inkant1990/com/lonelyfinder/screen/map/MapActivity.kt
@@ -2,6 +2,8 @@ package inkant1990.com.lonelyfinder.screen.map
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 import inkant1990.com.lonelyfinder.R
 import inkant1990.com.lonelyfinder.base.BaseMvvmActivity
 import inkant1990.com.lonelyfinder.databinding.ActivityMapBinding
@@ -22,6 +24,9 @@ class MapActivity : BaseMvvmActivity<
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment).getMapAsync {
+            it.mapType = GoogleMap.MAP_TYPE_NORMAL
+        }
     }
 
 
